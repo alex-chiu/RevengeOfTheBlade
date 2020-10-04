3bcefe1cb59bcaa8ba0f3cd1d2d73bb57b797021
Alex <alex.chiu27@gmail.com>
2020-10-04 17:54:26 -0500

    Boss Movement and Attack Code

--- ObsidianBones/Scene1.js
+++ ObsidianBones/Scene1.js
@@ -1,23 +1,22 @@
 // Global Variables
-var player, player_atk;
+var player, playerAtk;
 var boss;
-var boss_alive = true;
-var cursors;
-var spaceBar;
+var bossAlive = true;
+var playerAlive = true;
+var playerDetected = false;
+var delX;
+var laserGroup;
+var cursors, spaceBar;
 var W, A, S, D;
 var life = 100, bossLife = 100;
 var lifeText, bossLifeText;
-var attack_anim_playing = false;
-var sky;
-var clouds;
-var far;
-var back;
-var mid;
-var front;
+var attackAnimPlaying = false;
+var sky, clouds;
+var far, back, mid, front;
 var ground, platforms;
 
 // DEBUG PARAMETERS
-var debug = true;
+var debug = false;
 
 // Scene1 Class
 class Scene1 extends Phaser.Scene{
@@ -27,7 +26,7 @@ class Scene1 extends Phaser.Scene{
 
     // Preload Images and Sprites
     preload() {
-        this.load.spritesheet('robotBoss', 'assets/sprites/robot-boss-sprite.png', { frameWidth: 320, frameHeight: 500 });
+        this.load.spritesheet('robotBoss', 'assets/sprites/robot-boss-sprite.png', { frameWidth: 390, frameHeight: 500 });
         this.load.spritesheet('hero', 'assets/sprites/hero-walk-preattack-sprite.png', { frameWidth: 150, frameHeight: 230 });
         this.load.spritesheet('hero_attack', 'assets/sprites/hero-attack-sprite.png', { frameWidth: 255, frameHeight: 230 });
 
@@ -39,6 +38,9 @@ class Scene1 extends Phaser.Scene{
         this.load.image('mid4', 'assets/backgrounds/stage5/4mid.png');
         this.load.image('front5', 'assets/backgrounds/stage5/5front.png');
         this.load.image('ground', 'assets/backgrounds/stage5/6platform.png');
+
+        // Laser
+        this.load.image('laser', 'assets/laser.png')
     }
 
     // Create all the Sprites/Images/Platforms
@@ -65,8 +67,8 @@ class Scene1 extends Phaser.Scene{
         for (var i = 15; i <= 800; i += 30 ) {
             platforms.create(i, 565);
         }
-        if (!debug) {
-            platforms.children.visible = false;
+        if (debug == false) {
+            platforms.setVisible(false);
         }
 
         // Create Boss
@@ -77,6 +79,29 @@ class Scene1 extends Phaser.Scene{
         boss.scaleY = boss.scaleX;
         boss.body.setGravityY(300);
 
+        // Boss' Laser Attacks
+        laserGroup = new LaserGroup(this);
+
+        // Create Boss Animations
+        this.anims.create({
+            key:'bossLeftAtk',
+            frames: this.anims.generateFrameNumbers('robotBoss', { start: 0, end: 2}),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key:'bossRightAtk',
+            frames: this.anims.generateFrameNumbers('robotBoss', { start: 4, end: 6}),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key:'bossDefault',
+            frames: [ { key: 'robotBoss', frame: 3 } ],
+            frameRate: 10,
+            repeat: -1
+        });
+
         // Create Player
         player = this.physics.add.sprite(100, 475, 'hero');
         player.setBounce(0.25);
@@ -84,15 +109,14 @@ class Scene1 extends Phaser.Scene{
         player.displayWidth = game.config.width * 0.075;
         player.scaleY = player.scaleX;
         player.body.setGravityY(300);
-        player.setInteractive();
 
-        player_atk = this.physics.add.sprite(100, 475, 'hero_attack');
-        player_atk.setBounce(0.25);
-        player_atk.setCollideWorldBounds(true);
-        player_atk.displayWidth = game.config.width * 0.128;
-        player_atk.scaleY = player_atk.scaleX;
-        player_atk.body.setGravityY(300);
-        player_atk.visible = false;
+        playerAtk = this.physics.add.sprite(100, 475, 'hero_attack');
+        playerAtk.setBounce(0.25);
+        playerAtk.setCollideWorldBounds(true);
+        playerAtk.displayWidth = game.config.width * 0.128;
+        playerAtk.scaleY = playerAtk.scaleX;
+        playerAtk.body.setGravityY(300);
+        playerAtk.visible = false;
 
         // Create Player Animations
         this.anims.create({
@@ -113,25 +137,25 @@ class Scene1 extends Phaser.Scene{
             repeat: -1
         });
         this.anims.create({
-            key: 'pre_atk_l',
+            key: 'preAtkL',
             frames: this.anims.generateFrameNumbers('hero', { start: 0, end: 7 }),
             frameRate: 32,
             repeat: 0
         });
         this.anims.create({
-            key: 'pre_atk_r',
+            key: 'preAtkR',
             frames: this.anims.generateFrameNumbers('hero', { start: 21, end: 28 }),
             frameRate: 32,
             repeat: 0
         });
         this.anims.create({
-            key: 'atk_l',
+            key: 'playerAtkL',
             frames: this.anims.generateFrameNumbers('hero_attack', { start: 0, end: 5 }),
             frameRate: 15,
             repeat: 0
         });
         this.anims.create({
-            key: 'atk_r',
+            key: 'playerAtkR',
             frames: this.anims.generateFrameNumbers('hero_attack', { start: 6, end: 11 }),
             frameRate: 15,
             repeat: 0
@@ -147,25 +171,22 @@ class Scene1 extends Phaser.Scene{
 
         // Add Colliders
         this.physics.add.collider(player, platforms);
-        this.physics.add.collider(player_atk, platforms);
+        this.physics.add.collider(playerAtk, platforms);
         this.physics.add.collider(boss, platforms);
-
-
     }
 
     // Constantly Updating Game Loop
     update() {
         // Implement Parallax Background
-        clouds.tilePositionX -= 0.2;
-        far.tilePositionX += 0.5;
-        back.tilePositionX -= 0.7;
-        mid.tilePositionX += 1.0;
-
+        clouds.tilePositionX -= 0.5;
+        far.tilePositionX += 0.3;
+        back.tilePositionX -= 0.2;
+        mid.tilePositionX += 0.1;
 
         // Player Movement
         if (A.isDown) {
             player.setVelocityX(-160);
-            player_atk.setVelocityX(-160);
+            playerAtk.setVelocityX(-160);
             player.anims.play('left', true);
 
             front.tilePositionX -= 3;
@@ -173,7 +194,7 @@ class Scene1 extends Phaser.Scene{
         }
         else if (D.isDown) {
             player.setVelocityX(160);
-            player_atk.setVelocityX(160);
+            playerAtk.setVelocityX(160);
             player.anims.play('right', true);
 
             front.tilePositionX += 3;
@@ -181,35 +202,34 @@ class Scene1 extends Phaser.Scene{
         }
         else {
             player.setVelocityX(0);
-            player_atk.setVelocityX(0);
+            playerAtk.setVelocityX(0);
             player.anims.play('turn');
         }
 
         // Jumping
         if ((spaceBar.isDown || W.isDown) && player.body.touching.down) {
             player.setVelocityY(-250);
-            player_atk.setVelocityY(-250);
+            playerAtk.setVelocityY(-250);
         }
 
-                // Attack Animations
-        if (attack_anim_playing == false) {
+        // Attack Animations
+        if (attackAnimPlaying == false) {
             if (cursors.right.isDown) {
-                attack_anim_playing = true;
-                player.anims.play('pre_atk_r');
+                attackAnimPlaying = true;
+                player.anims.play('preAtkR');
                 this.time.addEvent({
                     delay: 250,
                     callback: () => {
                         player.visible = false;
-                        player_atk.visible = true;
-                        player_atk.anims.play('atk_r');
-                        this.updateLifeText(player_atk, boss);
+                        playerAtk.visible = true;
+                        playerAtk.anims.play('playerAtkR');
+                        this.updateBossLifeText(playerAtk, boss);
                         this.time.addEvent({
                             delay: 400,
                             callback: () => {
-                                player_atk.visible = false;
+                                playerAtk.visible = false;
                                 player.visible = true;
-                                attack_anim_playing = false;
-
+                                attackAnimPlaying = false;
                             }
                         })
                     }
@@ -217,41 +237,165 @@ class Scene1 extends Phaser.Scene{
             }
 
             else if (cursors.left.isDown) {
-                attack_anim_playing = true;
-                player.anims.play('pre_atk_l');
+                attackAnimPlaying = true;
+                player.anims.play('preAtkL');
                 this.time.addEvent({
                     delay: 250,
                     callback: () => {
                         player.visible = false;
-                        player_atk.visible = true;
-                        player_atk.anims.play('atk_l');
-                        this.updateLifeText(player_atk, boss);
+                        playerAtk.visible = true;
+                        playerAtk.anims.play('playerAtkL');
+                        this.updateBossLifeText(playerAtk, boss);
                         this.time.addEvent({
                             delay: 400,
                             callback: () => {
-                                player_atk.visible = false;
+                                playerAtk.visible = false;
                                 player.visible = true;
-                                attack_anim_playing = false;
+                                attackAnimPlaying = false;
                             }
                         })
                     }
                 })
             }
         }
+
+        // Boss AI/Movement
+        if (!playerDetected) {
+            boss.anims.play('bossDefault');
+        }
+        else {
+            delX = boss.body.position.x - player.body.position.x;
+            if (player.body.position.x < boss.body.position.x) {
+                boss.anims.play('bossLeftAtk');
+                if (delX > 150) {
+                    boss.setVelocityX(-40);
+                }
+                else if (delX < 140) {
+                    boss.setVelocityX(40);
+                }
+                else {
+                    boss.setVelocityX(0);
+                    this.shootLaser('L');
+                }
+            }
+            else if (player.body.position.x > boss.body.position.x) {
+                boss.anims.play('bossRightAtk');
+                if (delX < -150) {
+                    boss.setVelocityX(40);
+                }
+                else if (delX > -140) {
+                    boss.setVelocityX(-40);
+                }
+                else {
+                    boss.setVelocityX(0);
+                    this.shootLaser('R');
+                }
+            }
+        }
+
+        if (Math.abs(player.body.position.x - boss.body.position.x) <= 200) {
+            playerDetected = true;
+        }
+
+        // Debug Player and Boss Locations
+        if (debug) {
+            console.log("Player X Location:" + player.body.position.x);
+            console.log("Boss X Location:" + boss.body.position.x);
+        }
+
+        this.updatePlayerLifeText();
     }
 
-    // Updates Life Text
-    updateLifeText(player_atk, boss){
-        var boundsA = player_atk.getBounds();
+    // Function that Updates the Boss' Life Text
+    updateBossLifeText(playerAtk, boss) {
+        var boundsA = playerAtk.getBounds();
         var boundsB = boss.getBounds();
 
-        if ((Phaser.Geom.Rectangle.Overlaps(boundsA, boundsB) == true) && boss_alive){
-            bossLife-=10
+        if ((Phaser.Geom.Rectangle.Overlaps(boundsA, boundsB)) && bossAlive) {
+            bossLife -= 10
             bossLifeText.setText('Boss Life: ' + bossLife);
-            }
-        if (bossLife == 0){
+        }
+        if (bossLife == 0) {
             boss.disableBody(true, true);
-            boss_alive = false;
+            bossAlive = false;
+        }
+    }
+
+    // Function that Updates the Player's Life Text
+    updatePlayerLifeText() {
+        lifeText.setText('Life: ' + life);
+    }
+
+    shootLaser(direction) {
+        if (direction == 'L') {
+            laserGroup.fireLaser(boss.body.position.x, boss.body.position.y + 82, direction);
+        }
+        else {
+            laserGroup.fireLaser(boss.body.position.x + 100, boss.body.position.y + 110, direction);
+        }
+    }
+}
+
+// Laser Group Class
+class LaserGroup extends Phaser.Physics.Arcade.Group {
+    constructor(scene) {
+        super(scene.physics.world, scene);
+
+        this.createMultiple({
+            classType: Laser,
+            frameQuantity: 1, 
+            active: false,
+            visible: false,
+            key: 'laser'
+        })
+    }
+
+    fireLaser (x, y, direction) {
+        const laser = this.getFirstDead(false);
+        if (laser) {
+            laser.fire(x, y, direction);
+        }
+    }
+}
+
+// Laser Class
+class Laser extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y) {
+        super(scene, x, y, 'laser');
+    }
+
+    preUpdate(time, delta) {
+        super.preUpdate(time, delta);
+
+        if (this.y >= 528) {
+            this.setActive(false);
+            this.setVisible(false);
+        }
+        else if (Phaser.Geom.Rectangle.Overlaps(this.getBounds(), player.getBounds()) && playerAlive) {
+            life -= 5;
+            this.setActive(false);
+            this.setVisible(false);
+        }
+        if (life == 0) {
+            player.disableBody(true, true);
+            player.setActive(false);
+            player.setVisible(false);
+            playerAlive = false;
+        }
+    }
+
+    fire (x, y, direction) {
+        this.body.reset(x, y);
+        this.setActive(true);
+        this.setVisible(true);
+        this.body.setGravityY(0);
+
+        if (direction == 'L') {
+            this.setVelocityX(-250);
+            this.setVelocityY(100);
+        }
+        else if (direction == 'R') {
+            this.setVelocityX(250);
         }
     }
 }
