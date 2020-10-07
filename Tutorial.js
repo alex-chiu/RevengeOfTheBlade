3922f1c1d2a838814e827d77bf388b94dc70a617
Alex <alex.chiu27@gmail.com>
2020-10-07 16:05:28 -0500

    Create Tutorial Scene

--- Tutorial.js
+++ Tutorial.js
@@ -1,15 +1,11 @@
 // Global Variables
 var player, playerAtk;
-var boss;
-var bossAlive = true;
 var playerAlive = true;
-var playerDetected = false;
 var delX;
-var laserGroup;
 var cursors, spaceBar;
 var W, A, S, D;
 var life = 100, bossLife = 100;
-var lifeText, bossLifeText;
+var lifeText;
 var attackAnimPlaying = false;
 var sky, clouds;
 var far, back, mid, front;
@@ -19,14 +15,13 @@ var ground, platforms;
 var debug = false;
 
 // Robot Boss Fight Class
-class RobotBossFight extends Phaser.Scene{
+class Tutorial extends Phaser.Scene{
     constructor() {
-        super({ key: 'RobotBossFight' });
+        super({ key: 'Tutorial' });
     }
 
     // Preload Images and Sprites
     preload() {
-        this.load.spritesheet('robotBoss', 'assets/sprites/robot-boss-sprite.png', { frameWidth: 390, frameHeight: 500 });
         this.load.spritesheet('hero', 'assets/sprites/hero-walk-preattack-sprite.png', { frameWidth: 150, frameHeight: 230 });
         this.load.spritesheet('hero_attack', 'assets/sprites/hero-attack-sprite.png', { frameWidth: 255, frameHeight: 230 });
 
@@ -38,9 +33,6 @@ class RobotBossFight extends Phaser.Scene{
         this.load.image('mid4', 'assets/backgrounds/stage5/4mid.png');
         this.load.image('front5', 'assets/backgrounds/stage5/5front.png');
         this.load.image('ground', 'assets/backgrounds/stage5/6platform.png');
-
-        // Laser
-        this.load.image('laser', 'assets/laser.png')
     }
 
     // Create all the Sprites/Images/Platforms
@@ -60,7 +52,6 @@ class RobotBossFight extends Phaser.Scene{
 
         // Text
         lifeText = this.add.text(15, 15, 'Life: 100', { fontSize: '25px', fill: '#ffffff' });
-        bossLifeText = this.add.text(580, 15, 'Boss Life: 100', { fontSize: '25px', fill: '#ffffff' });
 
         // Platforms
         platforms = this.physics.add.staticGroup();
@@ -70,37 +61,6 @@ class RobotBossFight extends Phaser.Scene{
         if (debug == false) {
             platforms.setVisible(false);
         }
-
-        // Create Boss
-        boss = this.physics.add.sprite(650, 400, 'robotBoss')
-        boss.setBounce(0);
-        boss.setCollideWorldBounds(true);
-        boss.displayWidth = game.config.width * 0.15;
-        boss.scaleY = boss.scaleX;
-        boss.body.setGravityY(300);
-
-        // Boss' Laser Attacks
-        laserGroup = new LaserGroup(this);
-
-        // Create Boss Animations
-        this.anims.create({
-            key:'bossLeftAtk',
-            frames: this.anims.generateFrameNumbers('robotBoss', { start: 0, end: 2}),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.anims.create({
-            key:'bossRightAtk',
-            frames: this.anims.generateFrameNumbers('robotBoss', { start: 4, end: 6}),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.anims.create({
-            key:'bossDefault',
-            frames: [ { key: 'robotBoss', frame: 3 } ],
-            frameRate: 10,
-            repeat: -1
-        });
 
         // Create Player
         player = this.physics.add.sprite(100, 475, 'hero');
@@ -172,7 +132,6 @@ class RobotBossFight extends Phaser.Scene{
         // Add Colliders
         this.physics.add.collider(player, platforms);
         this.physics.add.collider(playerAtk, platforms);
-        this.physics.add.collider(boss, platforms);
     }
 
     // Constantly Updating Game Loop
@@ -223,7 +182,6 @@ class RobotBossFight extends Phaser.Scene{
                         player.visible = false;
                         playerAtk.visible = true;
                         playerAtk.anims.play('playerAtkR');
-                        this.updateBossLifeText(playerAtk, boss);
                         this.time.addEvent({
                             delay: 400,
                             callback: () => {
@@ -245,7 +203,6 @@ class RobotBossFight extends Phaser.Scene{
                         player.visible = false;
                         playerAtk.visible = true;
                         playerAtk.anims.play('playerAtkL');
-                        this.updateBossLifeText(playerAtk, boss);
                         this.time.addEvent({
                             delay: 400,
                             callback: () => {
@@ -258,150 +215,9 @@ class RobotBossFight extends Phaser.Scene{
                 })
             }
         }
-
-        // Boss AI/Movement
-        if (!playerDetected) {
-            boss.anims.play('bossDefault');
-        }
-        else {
-            delX = boss.body.position.x - player.body.position.x;
-            if (player.body.position.x < boss.body.position.x) {
-                boss.anims.play('bossLeftAtk');
-                if (delX > 150) {
-                    boss.setVelocityX(-40);
-                }
-                else if (delX < 140) {
-                    boss.setVelocityX(40);
-                }
-                else {
-                    boss.setVelocityX(0);
-                    if (bossAlive){
-                      this.shootLaser('L');
-                    }
-
-                }
-            }
-            else if (player.body.position.x > boss.body.position.x) {
-                boss.anims.play('bossRightAtk');
-                if (delX < -150) {
-                    boss.setVelocityX(40);
-                }
-                else if (delX > -140) {
-                    boss.setVelocityX(-40);
-                }
-                else {
-                    boss.setVelocityX(0);
-                    if (bossAlive){
-                      this.shootLaser('R');
-                    }
-
-                }
-            }
-        }
-
-        if (Math.abs(player.body.position.x - boss.body.position.x) <= 200) {
-            playerDetected = true;
-        }
-
-        // Debug Player and Boss Locations
-        if (debug) {
-            console.log("Player X Location:" + player.body.position.x);
-            console.log("Boss X Location:" + boss.body.position.x);
-        }
-
-        this.updatePlayerLifeText();
     }
-
-    // Function that Updates the Boss' Life Text
-    updateBossLifeText(playerAtk, boss) {
-        var boundsA = playerAtk.getBounds();
-        var boundsB = boss.getBounds();
-
-        if ((Phaser.Geom.Rectangle.Overlaps(boundsA, boundsB)) && bossAlive) {
-            bossLife -= 10
-            bossLifeText.setText('Boss Life: ' + bossLife);
-        }
-        if (bossLife == 0) {
-            boss.disableBody(true, true);
-            bossAlive = false;
-        }
-    }
-
     // Function that Updates the Player's Life Text
     updatePlayerLifeText() {
         lifeText.setText('Life: ' + life);
-    }
-
-    shootLaser(direction) {
-        if (direction == 'L') {
-            laserGroup.fireLaser(boss.body.position.x, boss.body.position.y + 82, direction);
-        }
-        else {
-            laserGroup.fireLaser(boss.body.position.x + 100, boss.body.position.y + 110, direction);
-        }
-    }
-}
-
-// Laser Group Class
-class LaserGroup extends Phaser.Physics.Arcade.Group {
-    constructor(scene) {
-        super(scene.physics.world, scene);
-
-        this.createMultiple({
-            classType: Laser,
-            frameQuantity: 1,
-            active: false,
-            visible: false,
-            key: 'laser'
-        })
-    }
-
-    fireLaser (x, y, direction) {
-        const laser = this.getFirstDead(false);
-        if (laser) {
-            laser.fire(x, y, direction);
-        }
-    }
-}
-
-// Laser Class
-class Laser extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y) {
-        super(scene, x, y, 'laser');
-    }
-
-    preUpdate(time, delta) {
-        super.preUpdate(time, delta);
-
-        if (this.y >= 528) {
-            this.setActive(false);
-            this.setVisible(false);
-        }
-        else if (Phaser.Geom.Rectangle.Overlaps(this.getBounds(), player.getBounds()) && playerAlive) {
-            life -= 5;
-            this.setActive(false);
-            this.setVisible(false);
-        }
-        if (life == 0) {
-            player.disableBody(true, true);
-            player.setActive(false);
-            player.setVisible(false);
-            playerAlive = false;
-        }
-    }
-
-    fire (x, y, direction) {
-        this.body.reset(x, y);
-        this.setActive(true);
-        this.setVisible(true);
-        this.body.setGravityY(0);
-
-        if (direction == 'L') {
-            this.setVelocityX(-250);
-            this.setVelocityY(100);
-        }
-        else if (direction == 'R') {
-            this.setVelocityX(250);
-        }
     }
 }
