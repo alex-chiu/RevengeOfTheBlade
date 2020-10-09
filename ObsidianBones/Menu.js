128cc89072b36838948b2382ad105353a98207f9
EdoardoPalazzi <edoardo.palazzi@gmail.com>
2020-10-08 19:41:45 -0500

    background menu

--- ObsidianBones/Menu.js
+++ ObsidianBones/Menu.js
@@ -4,20 +4,25 @@ class Menu extends Phaser.Scene{
     }
 
     preload(){
-      this.add.text(80,100,'OBSIDIAN BONES', { fontSize: '75px', fill: '#ffffff' });
 
-      const tutorialButton = this.add.text(350, 250, 'TUTORIAL', { fontSize: '20px', fill: '#b5dbf7' });
-      tutorialButton.setInteractive();
-      tutorialButton.on('pointerdown', () => { this.scene.start('Tutorial1'); });
-
-      const playButton = this.add.text(310, 350, 'PLAY', { fontSize: '75px', fill: '#8db9d9' });
-      playButton.setInteractive();
-      playButton.on('pointerdown', () => { this.scene.start('RobotBossFight'); });
+      this.load.image('sky0', 'assets/backgrounds/stage5/0sky.png');
+      this.load.image('clouds1', 'assets/backgrounds/stage5/1clouds.png');
 
     }
 
     create(){
+      this.add.tileSprite(400, 300, 800, 600, 'sky0');
+      this.add.tileSprite(400, 300, 800, 600, 'clouds1');
 
+      this.add.text(80,100,'OBSIDIAN BONES', { fontSize: '75px', fill: '#ffffff' });
+
+      const tutorialButton = this.add.text(350, 300, 'TUTORIAL', { fontSize: '20px', fill: '#b5dbf7' });
+      tutorialButton.setInteractive();
+      tutorialButton.on('pointerdown', () => { this.scene.start('Tutorial1'); });
+
+      const playButton = this.add.text(310, 375, 'PLAY', { fontSize: '75px', fill: '#8db9d9' });
+      playButton.setInteractive();
+      playButton.on('pointerdown', () => { this.scene.start('RobotBossFight'); });
     }
 
     update(){
