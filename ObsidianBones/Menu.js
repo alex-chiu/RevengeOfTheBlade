1ae1127c69e5d989f052318a5ec6219ba18de09e
EdoardoPalazzi <edoardo.palazzi@gmail.com>
2020-10-08 19:30:34 -0500

    instructions page

--- ObsidianBones/Menu.js
+++ ObsidianBones/Menu.js
@@ -6,11 +6,11 @@ class Menu extends Phaser.Scene{
     preload(){
       this.add.text(80,100,'OBSIDIAN BONES', { fontSize: '75px', fill: '#ffffff' });
 
-      const tutorialButton = this.add.text(350, 250, 'TUTORIAL', { fontSize: '20px', fill: '#0f0' });
+      const tutorialButton = this.add.text(350, 250, 'TUTORIAL', { fontSize: '20px', fill: '#b5dbf7' });
       tutorialButton.setInteractive();
       tutorialButton.on('pointerdown', () => { this.scene.start('Tutorial1'); });
 
-      const playButton = this.add.text(310, 350, 'PLAY', { fontSize: '75px', fill: '#0f0' });
+      const playButton = this.add.text(310, 350, 'PLAY', { fontSize: '75px', fill: '#8db9d9' });
       playButton.setInteractive();
       playButton.on('pointerdown', () => { this.scene.start('RobotBossFight'); });
 
--- ObsidianBones/Tutorial1.js
+++ ObsidianBones/Tutorial1.js
@@ -5,17 +5,19 @@ class Tutorial1 extends Phaser.Scene{
     }
 
     preload(){
-      this.add.text(130,15,'INSTRUCTIONS', { fontSize: '75px', fill: '#8d948d' });
+      this.add.text(130,15,'INSTRUCTIONS', { fontSize: '75px', fill: '#6543b5' });
 
-      this.add.text(50,150,'W - jump', { fontSize: '40px', fill: '#ffffff' });
-      this.add.text(50,200,'A - move left', { fontSize: '40px', fill: '#ffffff' });
-      this.add.text(50,250,'D - move right', { fontSize: '40px', fill: '#ffffff' });
-      this.add.text(50,350,'<> - sword attacks', { fontSize: '40px', fill: '#ffffff' });
-      this.add.text(50,400,'   - daggers attack', { fontSize: '40px', fill: '#ffffff' });
+      this.add.text(50,150,'W - jump', { fontSize: '40px', fill: '#d2c4f5' });
+      this.add.text(50,200,'A - move left', { fontSize: '40px', fill: '#d2c4f5' });
+      this.add.text(50,250,'D - move right', { fontSize: '40px', fill: '#d2c4f5' });
+      this.add.text(50,350,'<> - sword attacks', { fontSize: '40px', fill: '#d2c4f5' });
+      this.add.text(50,400,'   - daggers attack', { fontSize: '40px', fill: '#d2c4f5' });
 
       this.load.image('hero', 'assets/sprites/hero.png');
       this.load.image('arrowR', 'assets/arrowR.png');
       this.load.image('arrowL', 'assets/arrowL.png');
+      this.add.text(100, 541,'MENU', { fontSize: '20px', fill: '#ffffff' });
+      this.add.text(605, 541,'PRACTICE', { fontSize: '20px', fill: '#ffffff' });
 
     }
 
